fa8dffa5fdf1531c3e6a5e7e7fd9f7ce6ed24996
Jonathan <jgopel@gmail.com>
2015-07-02 12:09:59 -0400

    Working multiplier and screen filler
    Screen filler is very slow due to choice to aim for more compact code. Not going to fix because of time constraints
    
    Signed-off-by: Jonathan <jgopel@gmail.com>

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -3,9 +3,95 @@
 // by Nisan and Schocken, MIT Press.
 // File name: projects/04/Fill.asm
 
-// Runs an infinite loop that listens to the keyboard input. 
+// Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel. When no key is pressed, the
 // program clears the screen, i.e. writes "white" in every pixel.
 
-// Put your code here.
+// Setup variables
+	@desired
+	M=0       // Using 0 for white
+	@current
+	M=0
+
+(START)
+	// Check keyboard
+	@KBD
+	D=M
+	@BLACKDESIRED
+	D;JNE
+
+	// Otherwise set white
+	@desired
+	M=0
+	@COLORPICKED
+	0;JMP
+
+(BLACKDESIRED)
+	@desired
+	M=1
+	@COLORPICKED
+	0;JMP
+
+(COLORPICKED)
+	// The color has been set check if it's different from current color
+	@desired
+	D=M
+	@current
+	D=D-M
+	@CHANGESCREEN
+	D;JNE
+
+	// Otherwise start over
+	@START
+	0;JMP
+
+(CHANGESCREEN)
+	// Set current to desired
+	@desired
+	D=M
+	@current
+	M=D
+
+	// Determine what color to set
+	@desired
+	D=M
+	@BLACKSCREEN
+	D;JNE
+
+	// Set white if no jump
+	@color
+	M=0
+
+(SETSCREEN)
+	@KBD
+	D=A
+	@i
+	M=D-1
+(SCREENLOOP)
+	@color
+	D=M
+	@i
+	A=M
+	M=D
+
+	// Iterate
+	@SCREEN
+	D=M
+	@i
+	M=M-D
+	M=M-1
+	D=M
+	@SCREENLOOP
+	D;JNE
+
+	@START
+	0;JMP
+
+(BLACKSCREEN)
+	//
+	@color
+	M=-1
+
+	@SETSCREEN
+	0;JMP
--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -6,4 +6,40 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+// Setup variables
+@R2
+M=0
+
+// Initial checks
+@R0
+D=M
+@END
+D;JEQ
+
+@R1
+D=M
+@END
+D;JEQ
+
+(ADDLOOP)
+	// Check for exit condition
+	@R1
+	D=M
+	@END
+	D;JEQ
+
+	@R0
+	D=M
+	@R2
+	M=D+M
+
+	@R1
+	M=M-1
+
+	// Iterate
+	@ADDLOOP
+	0;JMP
+
+(END)
+	@END
+	0;JMP
