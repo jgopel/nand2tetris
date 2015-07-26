c07e7bb879e3a83f6464b572415a5a821059195a
Jonathan <jgopel@gmail.com>
2015-07-26 18:10:19 -0400

    Fixed logic errors
    Signed-off-by: Jonathan <jgopel@gmail.com>

--- 07/vm.c
+++ 07/vm.c
@@ -8,18 +8,18 @@
 
 int main( int argc, char *argv[] ) {
 	char *filename;
-	const char default_filename[] = "test.vm";
+	const char default_filename[] = "test";
 
 	if ( argc < 2 ) {
 		filename = malloc( strlen( default_filename ) + 1 );
 		strcpy( filename, default_filename );
 	} else {
 		filename = malloc( strlen( argv[ 1 ] ) + 1 );
-		strcpy( filename, default_filename );
+		strcpy( filename, argv[ 1 ] );
 	}
 
 	FILE *file_pointer = fopen( filename, "r" );
-	if ( file_pointer == NULL && strstr( filename, FILE_EXTENSION ) ) {
+	if ( file_pointer == NULL && ! strstr( filename, FILE_EXTENSION ) ) {
 		filename = realloc( filename, strlen( filename ) + strlen( FILE_EXTENSION ) + 1 );
 		strcat( filename, FILE_EXTENSION );
 
