61f8fb0b9b0f648061bf1646250e8d2811eb04ea
Jonathan <jgopel@gmail.com>
2015-07-27 13:32:29 -0400

    Added file creation for output file
    Signed-off-by: Jonathan <jgopel@gmail.com>

--- 07/vm.c
+++ 07/vm.c
@@ -3,22 +3,37 @@
 #include <string.h>
 #include "jg-files.h"
 
-#define FILE_EXTENSION ".vm"
+#define INPUT_EXTENSION ".vm"
+#define OUTPUT_EXTENSION ".asm"
 
 char* open_file_from_args( const int, char* * const, FILE* * const );
+char* replace_extension( char* const, char* const );
 
 int main( int argc, char* argv[] ) {
 	FILE* file_pointer = NULL;
-	char* filename = open_file_from_args( argc, argv, &file_pointer );
+	char* input_filename = open_file_from_args( argc, argv, &file_pointer );
 
 	if ( file_pointer == NULL ) {
-		printf( "%s does not exist.", filename );
+		printf( "%s does not exist.\n", input_filename );
 		return 0;
 	} else {
-		printf( "%s opened", filename );
+		printf( "%s opened\n", input_filename );
 	}
 
 	fclose( file_pointer );
+
+	char* output_filename = replace_extension( input_filename, OUTPUT_EXTENSION );
+	free( input_filename );
+	file_pointer = fopen( output_filename, "w" );
+
+	if ( file_pointer == NULL ) {
+		printf( "%s could not be created\n", output_filename );
+	} else {
+		printf( "%s created\n", output_filename );
+	}
+
+	fclose( file_pointer );
+
 	return 0;
 }
 
@@ -34,7 +49,23 @@ char* open_file_from_args( const int argument_count, char ** const argument_valu
 		strcpy( filename, argument_value[ 1 ] );
 	}
 
-	filename = open_file_with_extension( filename, FILE_EXTENSION, "r", fp );
+	char* output = open_file_with_extension( filename, INPUT_EXTENSION, "r", fp );
+	free( filename );
+
+	return output;
+}
+
+char* replace_extension( char* const input_filename, char* const new_extension ) {
+	char* filename = malloc( strlen( input_filename ) + 1 );
+	strcpy( filename, input_filename );
+
+	char *dot_position = strrchr( filename, '.' );
+	if ( dot_position != NULL ) {
+		*dot_position = '\0';
+	}
+
+	filename = realloc( filename, strlen( filename ) + strlen( new_extension ) + 1 );
+	filename = strcat( filename, new_extension );
 
 	return filename;
 }
