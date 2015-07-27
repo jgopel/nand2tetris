a5cde8ae2c806b8532291c1e5c2887d13c299a51
Jonathan <jgopel@gmail.com>
2015-07-27 12:53:29 -0400

    Reworked placement of pointers
    Signed-off-by: Jonathan <jgopel@gmail.com>

--- 07/vm.c
+++ 07/vm.c
@@ -6,12 +6,12 @@
 
 #define FILE_EXTENSION ".vm"
 
-char *open_file_from_args( const int, char ** const, FILE ** const );
-char *open_file( char*, char * const, FILE ** const );
+char* open_file_from_args( const int, char* * const, FILE* * const );
+char* open_file( char*, char* const, FILE* * const );
 
-int main( int argc, char *argv[] ) {
-	FILE *file_pointer = NULL;
-	char *filename = open_file_from_args( argc, argv, &file_pointer );
+int main( int argc, char* argv[] ) {
+	FILE* file_pointer = NULL;
+	char* filename = open_file_from_args( argc, argv, &file_pointer );
 
 	if ( file_pointer == NULL ) {
 		printf( "%s does not exist.", filename );
@@ -24,8 +24,8 @@ int main( int argc, char *argv[] ) {
 	return 0;
 }
 
-char *open_file_from_args( const int argument_count, char ** const argument_value, FILE ** const fp ) {
-	char *filename;
+char* open_file_from_args( const int argument_count, char ** const argument_value, FILE ** const fp ) {
+	char* filename;
 	const char default_filename[] = "test";
 
 	if ( argument_count < 2 ) {
@@ -41,7 +41,7 @@ char *open_file_from_args( const int argument_count, char ** const argument_valu
 	return filename;
 }
 
-char *open_file( char *filename, char * const file_extension, FILE ** const fp ) {
+char* open_file( char* filename, char* const file_extension, FILE* * const fp ) {
 	*fp = fopen( filename, "r" );
 	if ( *fp == NULL && ! strstr( filename, file_extension ) ) {
 		filename = realloc( filename, strlen( filename ) + strlen( file_extension ) + 1 );
